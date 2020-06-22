22cd5c3ef2043b8ac1095259aace36db3e331d03
webersab <sa_bine_weber@yahoo.de>
2020-06-22 11:27:15 +0200

    changed windows path to path

--- Multi-WikiRE/namanda_ZSHOT/NAMANDA/namanda/__init__.py
+++ Multi-WikiRE/namanda_ZSHOT/NAMANDA/namanda/__init__.py
@@ -2,14 +2,14 @@
 
 import os
 import sys
-from pathlib import WindowsPath
+from pathlib import Path,WindowsPath
 
 if sys.version_info < (3, 5):
     raise RuntimeError('NAMANDA supports Python 3.5 or higher.')
 
 DATA_DIR = (
     os.getenv('NAMANDA_DATA') or
-    os.path.join(WindowsPath(__file__).absolute().parents[1].as_posix(), 'data')
+    os.path.join(Path(__file__).absolute().parents[1].as_posix(), 'data')
 )
 
 from . import tokenizers
